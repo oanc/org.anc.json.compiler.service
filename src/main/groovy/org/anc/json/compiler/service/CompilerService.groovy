cd0725f6386c2763b4ef6b1338aacfb8f4dad379
Keith Suderman <suderman@cs.vassar.edu>
2017-06-14 15:27:13 -0400

    Accepts text/plain instead of json

--- src/main/groovy/org/anc/json/compiler/service/CompilerService.groovy
+++ src/main/groovy/org/anc/json/compiler/service/CompilerService.groovy
@@ -6,6 +6,7 @@ import org.anc.web.WebUtil
 
 import javax.ws.rs.Consumes
 import javax.ws.rs.DefaultValue
+import javax.ws.rs.GET
 import javax.ws.rs.POST
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
@@ -25,7 +26,7 @@ class CompilerService {
         compiler = new SchemaCompiler()
     }
     @POST
-    @Consumes(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.TEXT_PLAIN )
     @Produces(MediaType.APPLICATION_JSON)
     Response compile(
             @QueryParam('draft')
@@ -34,13 +35,15 @@ class CompilerService {
             @QueryParam('pretty')
             @DefaultValue('true')
             String prettyPrint,
-            String inputJson) {
+            String inputJson)
+    {
         try {
             compiler.draftVersion = draft as int
         }
         catch (NumberFormatException e) {
             return WebUtil.error("Invalid draft version specified. The draft version must be an integer value.")
         }
+
         if (prettyPrint == 'true') {
             compiler.prettyPrint = true
         }
@@ -52,12 +55,17 @@ class CompilerService {
         }
 
         try {
-            compiler.prettyPrint = prettyPrint == 'true'
             String outputJson = compiler.compile(inputJson)
             return WebUtil.json(outputJson)
         }
         catch (SchemaException e) {
             return WebUtil.error(e.getMessage())
         }
+    }
+
+    @GET
+    @Path("version")
+    Response version() {
+        return WebUtil.ok(Version.getVersion())
     }
 }
